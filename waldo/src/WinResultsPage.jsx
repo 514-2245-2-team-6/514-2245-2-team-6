4cb2b80ca2fa4f5d78d7738ba6580f6fb4b6da63
Toula Elwell <tae9467@rit.edu>
2025-03-21 20:36:15 -0400

    working on the game page

--- waldo/src/WinResultsPage.jsx
+++ waldo/src/WinResultsPage.jsx
@@ -1,16 +1,23 @@
 import './WinResultsPage.css'
 import './App.jsx'
+import waldo from './assets/waldo.png'
+import { Link } from "react-router-dom";
 
 function WinResultsPage() {
   return (
     <div className='winResultsPage'>
-      <h1>Your Results</h1>
-      <h2>Great Job, You Found Waldo! Congrats!!</h2>
-      <p>Your Score: <span>350</span></p>
-      <p>Your Streak: 6</p>
-      <p>Time Taken: 8 seconds</p>
-      <p>Accuracy: 100%</p>
-      <a href='' title='Click to play again'>Play Again</a>
+      <section className='results'>
+        <h1>Your Results</h1>
+        <h2>Great Job, You Found Waldo! Congrats!!</h2>
+        <p>Your Score: <span>350</span></p>
+        <p>Your Streak: 6</p>
+        <p>Time Taken: 8 seconds</p>
+        <p>Accuracy: 100%</p>
+        <Link to='/' title='Click to play again'>Play Again</Link>
+        <img src={waldo} alt='clipart of waldo'></img>
+      </section>
+      <div className='redStripee'></div>
+      <div className='redStripee'></div>
     </div>
   )
 }
