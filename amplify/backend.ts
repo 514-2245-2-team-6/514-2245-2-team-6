13fdfa8e9f5213384c5536622cb4d4d887a84186
Thomas <77514919+TB516@users.noreply.github.com>
2025-03-24 19:09:32 -0400

    Defined storage in backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,7 @@
-import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend } from "@aws-amplify/backend";
+import { storage } from "./storage/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({});
+defineBackend({ storage });
