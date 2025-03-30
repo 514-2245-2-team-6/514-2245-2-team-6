1d8b67fa095732e867896f1dfceb9811190b742c
Thomas <77514919+TB516@users.noreply.github.com>
2025-03-30 15:56:26 -0400

    Updated to use node for file system
    Allows for sandbox dev on windows

--- amplify/functions/getCroppedFace/resource.ts
+++ amplify/functions/getCroppedFace/resource.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from "node:url";
 import { defineFunction } from "@aws-amplify/backend";
 import { DockerImage, Duration } from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as fs from "fs";
 
 const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
@@ -19,9 +20,29 @@ export const getCroppedFaceFunctionHandler = defineFunction(
           local: {
             tryBundle(outputDir: string) {
               execSync(
-                `python3 -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
+                `python -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
               );
-              execSync(`cp -r ${functionDir}/* ${path.join(outputDir)}`);
+              const copyFiles = (source: string, destination: string) => {
+                fs.readdirSync(source).forEach((file) => {
+                  const sourcePath = path.join(source, file);
+                  const destinationPath = path.join(destination, file);
+                  const stat = fs.statSync(sourcePath);
+                  if (stat.isDirectory()) {
+                    // Create the directory if it doesn't exist
+                    if (!fs.existsSync(destinationPath)) {
+                      fs.mkdirSync(destinationPath);
+                    }
+                    // Recursively copy the sub-directory
+                    copyFiles(sourcePath, destinationPath);
+                  } else {
+                    // Copy the file
+                    fs.copyFileSync(sourcePath, destinationPath);
+                  }
+                });
+              };
+
+              // Run the copy process
+              copyFiles(functionDir, outputDir);
               return true;
             },
           },
