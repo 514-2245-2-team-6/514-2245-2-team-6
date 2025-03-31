be63c1b795bbd86958fad41b9fb0a6215c720f15
alexcarron <thealexcarron@gmail.com>
2025-03-31 01:02:03 -0400

    Add correct API Gateway base url

--- src/GameData/ApiGatewayBaseUrl.js
+++ src/GameData/ApiGatewayBaseUrl.js
@@ -1,1 +1,1 @@
-export const API_GATEWAY_BASE_URL = 'https://x7yh58arxj.execute-api.us-east-1.amazonaws.com/prod';
+export const API_GATEWAY_BASE_URL = 'https://qif1iwm888.execute-api.us-east-1.amazonaws.com/prod';
