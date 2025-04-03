3f61a1ac46cb4f64f5297bd2a06d1a2aff27a3a4
alexcarron <thealexcarron@gmail.com>
2025-04-02 22:27:11 -0400

    Add env variable to API GATEWAY URL

--- src/GameData/ApiGatewayBaseUrl.js
+++ src/GameData/ApiGatewayBaseUrl.js
@@ -1,1 +1,2 @@
-export const API_GATEWAY_BASE_URL = 'https://qif1iwm888.execute-api.us-east-1.amazonaws.com/prod';
+console.log(import.meta.env.VITE_API_GATEWAY_BASE_URL);
+export const API_GATEWAY_BASE_URL = import.meta.env.VITE_API_GATEWAY_BASE_URL;
